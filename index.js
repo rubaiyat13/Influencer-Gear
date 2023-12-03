729ea9cb7659e298fc1b5f612e2bd3d8e3f6fcbd
rubaiyat13 <md.rubaiyat.hasan.1@gmail.com>
2023-12-03 19:00:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function show(){
+    alert("Thanks for buying");
+    document.getElementById('preebook').innerHTML="Rabbir button";
+    document.getElementById('preebook').style.backgroundColor="green";
+}
